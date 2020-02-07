5f1a432d3d59be7b8ad896978b06a4402058b666
duvu <hoaivubk@gmail.com>
2020-02-07 18:12:16 +0700

    init commit

---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'ui/LoginPage.dart';
+
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
